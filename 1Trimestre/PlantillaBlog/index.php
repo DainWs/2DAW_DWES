f9bc0acaeecb04bf8ca493b0eb59fc629c8bebab
DainWs <jose.ant.du@gmail.com>
2021-11-13 18:42:14 +0100

    changes

--- 1Trimestre/PlantillaBlog/index.php
+++ 1Trimestre/PlantillaBlog/index.php
@@ -6,147 +6,73 @@ const DATA = [
 ];
 
 ?>
-<html class="v2 js" dir="ltr" lang="es">
+<html class="v2 js" lang="es">
+
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0" name="viewport">
 	<link href="css/main.css" rel="stylesheet" type="text/css">
-	<title></title>
+	<title><?= DATA['title'] ?></title>
 </head>
+
 <body class=" index">
 	<div class="site-wrapper">
-		<header class="site-header" role="banner">
-			<div class="header section" id="header">
-				<div class="widget Header" data-version="1" id="Header1">
+		<header class="site-header">
+			<div class="header section">
+				<div class="widget Header">
 					<h1 class="site-title">
 						<a href="index.php" rel="home" title="Ir al inicio">Home</a>
 					</h1>
-					<p class="description">
-						Creada por <a href="http://emiliocobos.net/"
-							title="Diseño y desarrollo web, blogger y demás">Emilio Cobos</a>
-					</p>
 				</div>
 			</div>
 		</header>
-		<div class="content-wrapper">
-			<div class="content" id="content" role="main">
-				<div class="main section" id="main">
-					<div class="widget Blog" data-version="1" id="Blog1">
+		<article class="content-wrapper">
+			<div class="content">
+				<div class="main section">
+					<div class="widget Blog">
 						<div class="blog-posts hfeed">
 							<div class="post-outer post-numero-0">
-								<div class="post hentry" id="651847362225055411" itemscope="itemscope"
-									itemtype="http://schema.org/BlogPosting">
-									<h2 class="post-title entry-title" itemprop="name">
-										<a href="http://lienzo-emiliocobos.blogspot.com/2013/01/estilo-1.html">Estilo
-											1</a>
-									</h2>
+								<div class="post hentry">
 									<header class="post-header">
-										<span class="author vcard post-author" itemprop="author" itemscope="itemscope"
-											itemtype="http://schema.org/Person">
-											<a class="url fn"
-												href="https://www.blogger.com/profile/01372442033006785572"
-												itemprop="name url" rel="author" title="Ver el perfil de Unknown">
-												Unknown
-											</a>
-										</span>
-										<time class="post-published published updated"
-											datetime="2013-01-09T16:27:00-08:00" itemprop="datePublished">
-											16:27
-										</time>
-										<span class="post-comment-link">
-											<a class="comment-link"
-												href="http://lienzo-emiliocobos.blogspot.com/2013/01/estilo-1.html#comment-form"
-												onclick="">
-												1 comentario:
-											</a>
-										</span>
+										<h2 class="post-title entry-title">Seccion 1</h2>
 									</header>
-									<div class="post-body entry-content" id="post-body-651847362225055411"
-										itemprop="articleBody">
-										Deberías de estar viendo una versión de la página algo más retocada ;)
-										<div class="jump-link">
-											<a href="http://lienzo-emiliocobos.blogspot.com/2013/01/estilo-1.html#more"
-												rel="bookmark" title="Estilo 1">Leer más »</a>
-										</div>
+									<div class="post-body entry-content">
+										
 									</div>
 									<footer class="post-footer">
+
 									</footer>
 								</div>
 							</div>
 							<div class="post-outer post-numero-1">
-								<div class="post hentry" id="6858873271733162530" itemscope="itemscope"
-									itemtype="http://schema.org/BlogPosting">
-									<h2 class="post-title entry-title" itemprop="name">
-										<a
-											href="http://lienzo-emiliocobos.blogspot.com/2013/01/este-es-el-titulo-de-un-post.html">Éste
-											es el título de un post</a>
-									</h2>
+								<div class="post hentry">
 									<header class="post-header">
-										<span class="author vcard post-author" itemprop="author" itemscope="itemscope"
-											itemtype="http://schema.org/Person">
-											<a class="url fn"
-												href="https://www.blogger.com/profile/01372442033006785572"
-												itemprop="name url" rel="author" title="Ver el perfil de Unknown">
-												Unknown
-											</a>
-										</span>
-										<time class="post-published published updated"
-											datetime="2013-01-09T15:56:00-08:00" itemprop="datePublished">
-											15:56
-										</time>
-										<span class="post-tags">
-											<a href="http://lienzo-emiliocobos.blogspot.com/search/label/Etiqueta%201"
-												itemprop="keywords" rel="tag">Etiqueta 1</a>,
-											<a href="http://lienzo-emiliocobos.blogspot.com/search/label/Etiqueta%202"
-												itemprop="keywords" rel="tag">Etiqueta 2</a>
-										</span>
-										<span class="post-comment-link">
-											<a class="comment-link"
-												href="http://lienzo-emiliocobos.blogspot.com/2013/01/este-es-el-titulo-de-un-post.html#comment-form"
-												onclick="">
-												No hay comentarios:
-											</a>
-										</span>
+										<h2 class="post-title entry-title">Post</h2>
 									</header>
-									<div class="post-body entry-content" id="post-body-6858873271733162530"
-										itemprop="articleBody">
+									<div class="post-body entry-content">
 										Lorem ipsum dolor sit amet, donec diam urna et wisi aliquam quis, ut habitasse
 										aenean dui amet et. Ullamcorper augue. Et mauris nunc ut morb...
-										<div class="jump-link">
-											<a href="http://lienzo-emiliocobos.blogspot.com/2013/01/este-es-el-titulo-de-un-post.html#more"
-												rel="bookmark" title="Éste es el título de un post">Leer más »</a>
-										</div>
 									</div>
 									<footer class="post-footer">
 									</footer>
 								</div>
-							</div>
-						</div>
-						<div class="blog-pager" id="blog-pager">
-							<a class="home-link" href="http://lienzo-emiliocobos.blogspot.com/" rel="home">Inicio</a>
-						</div>
-						<div class="blog-feeds">
-							<div class="feed-links">
-								Suscribirse a:
-								<a class="feed-link" href="http://lienzo-emiliocobos.blogspot.com/feeds/posts/default"
-									target="_blank" type="application/atom+xml">Entradas (Atom)</a>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="sidebar section" id="sidebar">
-				<div class="widget HTML" data-version="1" id="HTML2">
+			<aside class="sidebar section">
+				<div class="widget HTML">
 					<h2 class="title">Un widget en la sidebar</h2>
 					<div class="widget-content">
 						Esto puede ser cualquier otra cosa.
 					</div>
 				</div>
-			</div>
-		</div>
+			</aside>
+		</article>
 		<footer class="site-footer">
-			<div class="footer section" id="footer">
-				<div class="widget HTML" data-version="1" id="HTML3">
+			<div class="footer section">
+				<div class="widget HTML">
 					<h2 class="title">Ahora uno en el footer</h2>
 					<div class="widget-content">
 						Cualquier cosa
