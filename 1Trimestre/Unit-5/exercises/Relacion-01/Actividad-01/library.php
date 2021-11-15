76891ac5dd8050a6541e74389a0f642b754130fd
DainWs <jose.ant.du@gmail.com>
2021-11-15 12:50:47 +0100

    POO objects

---
+++ 1Trimestre/Unit-5/exercises/Relacion-01/Actividad-01/library.php
@@ -0,0 +1,7 @@
+<?php 
+
+Const DOGS = [
+    'Labrador' => new Perro('Labrador', 'Labrador', 60, 'gray'),
+    'Caniche' => new Perro('Caniche', 'Caniche', 10, 'brown'),
+    'Pastor' => new Perro('Pastor', 'Pastor', 80, 'brown')
+];
