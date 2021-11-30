ee917f2e8aaeb56bfd6db4b5b2216dc0e46c6b87
DainWs <jose.ant.du@gmail.com>
2021-11-30 09:07:08 +0100

    PDO DB Access

--- 1Trimestre/Unit-6/Examples/AccesoDBO/index.php
+++ 1Trimestre/Unit-6/Examples/AccesoDBO/index.php
@@ -10,36 +10,14 @@ try {
     $db = new PDO($url, DB_USERNAME, DB_PASSWORD);
     echo "conexion con exito";
 
+    $sql = 'SELECT * FROM USUARIOS';
+    $db->exec($sql);
 
+    $prepareSQL = $db->prepare('SELECT * FROM USUARIOS');
+    $prepareSQL->execute( array(0) );
+
+    var_dump($prepareSQL);
 
 } catch(Exception $e) {
     echo $e->getMessage();
-}
-
-
-echo "<br/><br/>";
-
-// Set Charset of Connection to UTF-8
-mysqli_query($connection, "SET NAME 'utf8'");
-
-//SQL Query
-$result = mysqli_query($connection, "SELECT * FROM usuarios");
-var_dump($result);
-echo "<br/><br/>";
-$usuarios = mysqli_fetch_assoc($result);
-var_dump($usuarios);
-
-echo "<br/><br/>";
-
-// Insert
-$insert = mysqli_query($connection, "INSERT INTO USUARIOS(nombre, apellidos, email, password) VALUES ('Javier', 'Sanchez Rueda', 'javierSanchezRueda@gmail.com', '12345678')");
-var_dump($insert);
-
-// Delete
-$delete = mysqli_query($connection, "DELETE FROM USUARIOS WHERE nombre='Javier'");
-if ($delete) {
-    echo "Se ha podido borrar correctamente";
-}
-else {
-    echo "Fallo en al borrar";
 }
