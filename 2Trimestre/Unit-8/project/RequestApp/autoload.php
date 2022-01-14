f041d4b75103152cda4a9d8d3b378b2bc21de3de
DainWs <jose.ant.du@gmail.com>
2022-01-14 14:08:45 +0100

    Autoload changes

--- 2Trimestre/Unit-8/project/RequestApp/autoload.php
+++ 2Trimestre/Unit-8/project/RequestApp/autoload.php
@@ -14,5 +14,8 @@ function autoloadDir($dir = __DIR__.'/src') {
 }
 
 spl_autoload_register(function($class) {
+    if (file_exists(__DIR__."\\$class.php")) {
+        include_once(__DIR__."\\$class.php");
+    }
     autoloadDir();
 });
